4f7c374adf55aeab3ced8544c2558b792ee89b8e
Maxmos1124 <luckyKaneshiro1124@gmail.com>
2024-05-10 03:59:26 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+<div align="center">
+
+  <h2 align="center">Dusan Dimic - Personal portfolio</h2>
+
+This website is fully responsive personal portfolio, <br />Responsive for all devices, built using Nextjs, TailwindCSS only.
+
+<a href="#"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br />
+
+### Demo Screeshots
+
+![Osama Portfolio Desktop Demo](./public/readme-images/portfolio.png "Desktop Demo")
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
+
+### Run Locally
+
+To run **portfolio** locally, run this command on your git bash:
+
+Linux and macOS:
+
+```bash
+sudo git clone https://github.com/Maxmos1124/portfolio.git
+```
+
+Windows:
+
+```bash
+git clone https://github.com/Maxmos1124/portfolio.git
+```
+
+### Contact
+
+If you want to contact with me you can reach me by using artoh1124@gmail.com.
+
+### License
+
+This project is **free to use** and does not contains any license.
---
+++ constants/constants.js
@@ -0,0 +1,83 @@
+export const NAME = 'Julio Acin';
+
+export const DESIGNATION = `Blockchain Developer and Smart Contract Auditor`;
+
+export const DETAILS = {
+  Residence: 'Mardrid',
+  City: 'Boadilla del Monte',
+  Age: '30',
+};
+
+export const SKILLS = [
+  {
+    title: 'Blockchain Developer',
+    level: '95%',
+  },
+  {
+    title: 'Smart Contract Auditor',
+    level: '20%',
+  },
+  {
+    title: 'MERN Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'Backend Developer',
+    level: '50%',
+  },
+  {
+    title: 'Frontend Developer',
+    level: '60%',
+  },
+  {
+    title: 'App Developer',
+    level: '40%',
+  },
+  // {
+  //   title: 'Technical Blogger',
+  //   level: '99%',
+  // },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  // 'JavaScript',
+  'NextJS',
+  'Python',
+  'Web3',
+  'Solidity',
+  // 'Kotlin',
+  // 'Android',
+  'Rust',
+  'C++',
+  // 'JAVA',
+  // 'TypeScript',
+  'NodeJS',
+  // 'Redux',
+  // 'Zustand',
+  // 'CSS',
+  // 'SCSS',
+  // 'TailwindCSS',
+  // 'Ant Design',
+  'Go',
+  // 'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  // 'Jira',
+  // 'Figma',
+  // 'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'noahsflood908@gmail.com',
+  PHONE: '+1 773 295 2131',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/mymiracle0118?tab=repositories&q=&type=&language=&sort=',
+  LINKEDIN: '',
+  TWITTER: '',
+  FACEBOOK: '',
+};
