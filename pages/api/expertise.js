afd41ff84b8f2569d7252baed8668ec4f55a2195
Maxmos <artoh1124@gmail.com>
2024-05-10 05:25:51 +0900

    Update: expertise.js

--- pages/api/expertise.js
+++ pages/api/expertise.js
@@ -11,25 +11,20 @@ const expertise = [
     },
     {
         id: 2,
-        title: 'Smart Contract Auditor',
-        desc: 'Now I am working as a warden in Code4rena and participate in contests and bug bounties. You can find me in leaderboard page in sherlock and code4rena.',
+        title: 'Frontend Development',
+        desc: 'I am familiar with React.js, Vue.js, Next.js and Tailwind CSS. I built lots of projects and received good reviews from the clients',
     },
     
-    // {
-    //     id: 2,
-    //     title: 'Designing (UI/UX)',
-    //     desc: 'I am proficient in using design software such as Figma, Adobe XD, and Adobe Illustrator. Through participating in hackathons and freelance work, I have gained practical experience in UI/UX design and graphic design. Designing is my passion, and it has allowed me to continue to develop my creative skills and grow in the field.',
-    // },
-    // {
-    //     id: 3,
-    //     title: 'Github',
-    //     desc: "GitHub is essential for version control and collaboration. Its intuitive interface and features like pull requests and issue tracking simplify the process. Continuous integration ensures up-to-date code. GitHub is vital to my success as a developer.",
-    // },
-    // {
-    //     id: 4,
-    //     title: 'Open Source Contributor',
-    //     desc: "Open Source is the future. I usually take some time on weekend and contribute into opensource project. It gives me opportunity to learn from best developer's practices and also gives me a chance to help others and contribute into the community for the good.",
-    // },
+    {
+        id: 3,
+        title: 'Designing (UI/UX)',
+        desc: 'I am proficient in using design software such as Figma, Adobe XD, and Adobe Illustrator. Through participating in hackathons and freelance work, I have gained practical experience in UI/UX design and graphic design. Designing is my passion, and it has allowed me to continue to develop my creative skills and grow in the field.',
+    },
+    {
+        id: 4,
+        title: 'Github',
+        desc: "GitHub is essential for version control and collaboration. Its intuitive interface and features like pull requests and issue tracking simplify the process. Continuous integration ensures up-to-date code. GitHub is vital to my success as a developer.",
+    }
 ]
 
 export default function handler(req, res) {
